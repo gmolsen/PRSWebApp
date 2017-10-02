700296cf99935817df844da4be058bc4578c58c7
Greg <gmolsenvideo@gmail.com>
2017-10-02 18:06:54 -0400

    AJAX, JSON, C#, EF, HTML, more

--- ng/JS/Users_List.js
+++ ng/JS/Users_List.js
@@ -2,14 +2,22 @@ $().ready (function() {
 
 $("#call").click(function() {
 
+
+		*/ ajax call is made to "List()" inside UsersController 
+		which then returns an array containing all properties for
+		each user (JSON)*/ 
 		$.getJSON(
 			"http://localhost:65241/Users/List/",
 			function(user) {
+			//jQuery command that clears content of table
  			$("tbody").empty();
+ 			// JSON user data is then printed as HTML in table format
  			for(var users of Users) {
  			var row = "<tr>" + "<td>" + users.UserName + "</td>";
  			row += "<td>" + users.Email + "</td>";
 			row += "</tr>";
+			/* jQuery command that appends formatted data into text of tag
+			containing id "tbody" in this case it is the body of a table*/ 
 				$("#tbody").append(row);
 			};
 			
